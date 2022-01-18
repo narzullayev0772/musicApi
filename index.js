42311cd413994d5095e9e85d668224fdaf4c5404
Developer0772 <alishernarzullayev7777@gmail.com>
2022-01-18 13:27:52 +0500

    fix json

--- index.js
+++ index.js
@@ -6,17 +6,13 @@ const dotenv = require('dotenv')
 dotenv.config({path:"./.env"})
 
 app.use(cors());
-/**
- * @param {string} stringDoc - string type
- *
- */
 
-app.get("/music/api", (req, res) => {
+app.get("/", (req, res) => {
   axios.get(process.env.URL)
   .then((data) =>{
     res.status(200).json({
         status:"success",
-        tracks: data.data.split("data-track=").map((e,index)=>index>0?e.split("data-title")[0]:null)
+        // tracks: data.data.split("data-track=").map((e,index)=>index>0?e.split("data-title")[0]:null)
     })
   }
   );
