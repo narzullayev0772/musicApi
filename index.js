4e83e2e191f5660859e30935fa5ea36c7f6cd7f3
Developer0772 <alishernarzullayev7777@gmail.com>
2022-01-18 16:26:41 +0500

    track id title added

--- index.js
+++ index.js
@@ -9,17 +9,25 @@ dotenv.config({path:"./.env"})
 app.use(cors());
 app.use(compression())
 
-// data-track="https://uzbmusic.net/uploads/files/jaloliddin_ahmadaliyev__sogindim.mp3"
-// data-title="Jaloliddin Ahmadaliyev - Sog&#039;indim" data-artist="Исполнитель"
-// data-img="/templates/YoqubovUz/dleimages/no_image.jpg">
+const trackCreator = (track,id)=>{
+  return {
+    id:id,
+    track:id?track.split("\ndata-title")[0]:"",
+    title:id?track.split(`data-title=`)[1].split("data-artist")[0]:""
 
+  }
+}
 app.get("/music/api", (req, res) => {
   axios.get(process.env.URL)
   .then((data) =>{
+    // {
+    //   track:data.data.split(`data-track=`)[1].split("\ndata-title")[0],
+    //   title:data.data.split(`data-title=`)[1].split("data-artist")[0]
+    // }
     res.status(200).json({
         status:"success",
         results:data.data.split(`class="track-item fx-row fx-middle js-item"`).length-1,
-        tracks:data.data.split(`class="track-item fx-row fx-middle js-item"`).map((e,index)=>index>0?e.split("data-artist")[0].replace("=",":").replace("=",":").replace("\n",``):null)
+        tracks: data.data.split(`data-track=`).map((e,index)=> trackCreator(e,index))
     })
   }
   );
