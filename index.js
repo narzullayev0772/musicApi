17e8962ec8b726b2e865201aa4e6b201daada043
Developer0772 <alishernarzullayev7777@gmail.com>
2022-01-18 15:06:08 +0500

    music author

--- index.js
+++ index.js
@@ -9,12 +9,17 @@ dotenv.config({path:"./.env"})
 app.use(cors());
 app.use(compression())
 
+// data-track="https://uzbmusic.net/uploads/files/jaloliddin_ahmadaliyev__sogindim.mp3"
+// data-title="Jaloliddin Ahmadaliyev - Sog&#039;indim" data-artist="Исполнитель"
+// data-img="/templates/YoqubovUz/dleimages/no_image.jpg">
+
 app.get("/music/api", (req, res) => {
   axios.get(process.env.URL)
   .then((data) =>{
     res.status(200).json({
         status:"success",
-        tracks: data.data.split("data-track=").map((e,index)=>index>0?e.split("data-title")[0]:null)
+        results:data.data.split(`class="track-item fx-row fx-middle js-item"`).length-1,
+        tracks:data.data.split(`class="track-item fx-row fx-middle js-item"`).map((e,index)=>index>0?e.split("data-artist")[0].replace("=",":").replace("=",":").replace("\n",``):null)
     })
   }
   );
